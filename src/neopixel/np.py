755eccb9ecf4593457eb7f459c388a0e85b3a83f
Rizky Ariestiyansyah <ariestiyansyah.rizky@gmail.com>
2018-09-09 19:34:35 +0700

    add rotate method

--- src/neopixel/np.py
+++ src/neopixel/np.py
@@ -1,4 +1,8 @@
+import logging
 import time
+
+
+log = logging.getLogger(__name__)
 
 
 REACTORS = range(1, 17)
@@ -25,6 +29,14 @@ def blink_enemy(np, delay=500, color=COLOR, end=7):
 	np.clear()
 
 
-def ultraman_mode(np):
-	blink_enemy(np, delay=250, color=0.0)
+def ultraman_mode(np, delay=250, color=0.0, end=7):
+	blink_enemy(np, delay=delay, color=color, end=end)
+
+def rotate(np, delay=DELAY, color=COLOR, saturation=SATURATION, brightness=BRIGHTNESS):
+	for reactor in REACTORS:
+		log.debug('Set: {} to color: {}'.format(reactor, color))
+		np.setHSB(reactor, color, saturation, brightness, 1, False)
+		np.show()
+		time.sleep_ms(delay)
+		np.clear()
 
